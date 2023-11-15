4a6d2ba6f55e3d8352026bbdb9b1c1f1159eb7aa
vrkakkara <vishnurajkakkara@gmail.com>
2023-11-14 22:11:04 -0500

    The code was missing in the first commit

--- src/AmusementParkQuestion/AmusementPark.java
+++ src/AmusementParkQuestion/AmusementPark.java
@@ -1,5 +1,25 @@
 package AmusementParkQuestion;
 
+
+
 public class AmusementPark {
+	
+	
+	int EnterHeight;
+	public static void main(String[] args) {
+		
+		AmusementPark entry=new AmusementPark();
+		entry.EnterHeight=120; //The height will be entered by the park authority here
+		
+		if(entry.EnterHeight>=90&&entry.EnterHeight<=200) {    //condition is established so that people between the height limit of 90 to 200cm can enter the park
+			
+			System.out.println("You can enter the park");
+		}
+		else {
+			System.out.println("You are not eligible to enter the park");
+		}
+	}
+
+	
 
 }
