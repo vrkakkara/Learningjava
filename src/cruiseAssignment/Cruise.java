3df1b8c1d50e247d5b2919426b96d3ca27aa536d
vrkakkara <vishnurajkakkara@gmail.com>
2023-12-26 17:50:09 -0500

    Assigment dated 21Dec2023

---
+++ src/cruiseAssignment/Cruise.java
@@ -0,0 +1,34 @@
+package cruiseAssignment;
+
+public class Cruise {
+	
+	public double finalBill(int numberOfAdults, int numberOfChildren, boolean hasFood) {
+		double grandTotal;
+		double buffetAdults=0;
+		double buffetKids=0;
+		double adultFare=numberOfAdults*43.99;
+		double childFare=numberOfChildren*12.99;
+		if(hasFood) {
+		buffetAdults=numberOfAdults*20.99;
+		buffetKids=numberOfChildren*4.99;
+		}
+		else {
+			buffetAdults=numberOfAdults*0;
+			buffetKids=numberOfChildren*0;
+		}
+		double totalFare=adultFare+childFare+buffetAdults+buffetKids;
+		double hst=totalFare*0.15;
+		grandTotal=totalFare+hst;
+		System.out.println("Your package includes: ");
+		System.out.println("Cruise Adults @"+numberOfAdults+" : "+adultFare);
+		System.out.println("Cruise Kids above age 5 @"+numberOfChildren+" : "+childFare);
+		System.out.println("Buffet Special Price Adults @"+numberOfAdults+" : "+buffetAdults);
+		System.out.println("Buffet Special Price Kids above age 5 @"+numberOfChildren+" : "+buffetKids);
+		System.out.println("Buffet Special Price Kids above age 5 @"+numberOfChildren+" : "+buffetKids);
+		System.out.println("Total price is: "+totalFare);
+		System.out.println("HST @15%: "+hst);
+		System.out.println("Final price is: "+grandTotal);
+		return grandTotal;
+	}
+
+}
---
+++ src/cruiseAssignment/MysteryCruise.java
@@ -0,0 +1,36 @@
+package cruiseAssignment;
+
+public class MysteryCruise extends Cruise{
+	@Override
+	public double finalBill(int numberOfAdults, int numberOfChildren, boolean hasFood) {
+		double grandTotal;
+		double buffetAdults=0;
+		double buffetKids=0;
+		double adultFare=numberOfAdults*45.99;
+		double childFare=numberOfChildren*12.99;
+		if(hasFood) {
+		buffetAdults=numberOfAdults*20.99;
+		buffetKids=numberOfChildren*4.99;
+		}
+		else {
+			buffetAdults=numberOfAdults*0;
+			buffetKids=numberOfChildren*0;
+		}
+		double totalFare=adultFare+childFare+buffetAdults+buffetKids;
+		double hst=totalFare*0.15;
+		grandTotal=totalFare+hst;
+		System.out.println("Your package includes: ");
+		System.out.println("Mystery Cruise Adults @"+numberOfAdults+" : "+adultFare);
+		System.out.println("Mystery Cruise Kids above age 5 @"+numberOfChildren+" : "+childFare);
+		System.out.println("Buffet Special Price Adults @"+numberOfAdults+" : "+buffetAdults);
+		System.out.println("Buffet Special Price Kids above age 5 @"+numberOfChildren+" : "+buffetKids);
+		System.out.println("Buffet Special Price Kids above age 5 @"+numberOfChildren+" : "+buffetKids);
+		System.out.println("Total price is: "+totalFare);
+		System.out.println("HST @15%: "+hst);
+		System.out.println("Final price is: "+grandTotal);
+		return grandTotal;
+	}
+	
+	
+
+}
